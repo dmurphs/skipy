a9e83fd4673c8192e4d5702a94bf60e7faa6e677
Daniel <d.p.murph19@gmail.com>
2017-03-25 16:22:19 -0600

    switched to generic views

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
-from .views import get_locations, get_all_location_data, get_location_data_for_year
+from .views import LocationList, LocationData
 
 urlpatterns = [
-    url(r'^locations/', get_locations),
-    url(r'^(?P<station_id>\w{0,7})/$', get_all_location_data),
-    url(r'^(?P<station_id>\w{0,7})/(?P<year>\w{4})/$', get_location_data_for_year)
+    url(r'^locations/', LocationList.as_view(), name='location-list'),
+    url(r'^(?P<station_id>\w{0,7})/(?P<year>\w{4})/$', LocationData.as_view(), name='location-data'),
 ]
--- apps/api/views.py
+++ apps/api/views.py
@@ -1,31 +1,24 @@
+from rest_framework import generics
+
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 
 from .models import NRCS_Locations, NRCS_MonthlySnow
 from .serializers import NRCS_LocationsSerializer, NRCS_MonthlySnowSerializer
 
-@api_view(['GET'])
-def get_locations(request):
-    '''Get all data collection sites
-       query: /api/locations'''
-    locations = NRCS_Locations.objects.all()
-    serializer = NRCS_LocationsSerializer(locations, many=True)
-    return JsonResponse(serializer.data, safe=False)
+class LocationList(generics.ListAPIView):
+    '''Get all locations'''
+    queryset = NRCS_Locations.objects.all()
+    serializer_class = NRCS_LocationsSerializer
 
-@api_view(['GET'])
-def get_all_location_data(request, station_id):
+class LocationData(generics.ListAPIView):
     '''Get all monthly snow data for location
-       example query: /api/12D10'''
-    location = NRCS_Locations.objects.get(pk=station_id)
-    all_snow_data = NRCS_MonthlySnow.objects.filter(location=location)
-    serializer = NRCS_MonthlySnowSerializer(all_snow_data, many=True)
-    return JsonResponse(serializer.data, safe=False)
+       example query: /api/12D10/1999'''
 
-@api_view(['GET'])
-def get_location_data_for_year(request, station_id, year):
-    '''Get data from specific year for location
-       example query: /api/12D10/1999)'''
-    location = NRCS_Locations.objects.get(pk=station_id)
-    year_snow_data = NRCS_MonthlySnow.objects.filter(location=location,water_year=year)
-    serializer = NRCS_MonthlySnowSerializer(year_snow_data, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    def get_queryset(self):
+        station_id = self.kwargs['station_id']
+        year = self.kwargs['year']
+        location = NRCS_Locations.objects.get(pk=station_id)
+        return NRCS_MonthlySnow.objects.filter(location=location,water_year=year)
+
+    serializer_class = NRCS_MonthlySnowSerializer
